79e413942857a00858b9a7746e21185d0d878e8f
Eric Schmidt <eric@eric.casa>
2022-12-05 15:33:49 +0100

    Completed Day 4

---
+++ Day4/Part2.tcl
@@ -0,0 +1,51 @@
+#!/usr/bin/env tclsh
+
+proc range2list {range} {
+    set tempList [split $range -]
+    set first [lindex $tempList 0]
+    set last [lindex $tempList 1]
+
+    set List ""
+
+    for {set index $first} {$index <= $last} {incr index} {
+        lappend List $index
+    }
+
+    return $List
+}
+
+set input [read [open "input.txt" r]]
+
+set Ergebnis 0
+
+foreach Pair $input {
+    set tempList [split $Pair ,]
+    set first [lindex $tempList 0]
+    set last [lindex $tempList 1]
+
+    set firstList [range2list $first]
+    set lastList [range2list $last]
+
+    set FirstInLast 0
+    set LastInFirst 0
+
+    foreach num $firstList {
+        if {[lsearch $lastList $num] > -1} {
+            set FirstInLast 1
+            break
+        }
+    }
+
+    foreach num $lastList {
+        if {[lsearch $firstList $num] > -1} {
+            set LastInFirst 1
+            break
+        }
+    }
+
+    if {$FirstInLast || $LastInFirst} {
+        incr Ergebnis
+    }
+}
+
+puts $Ergebnis
