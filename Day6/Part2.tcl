08ea7845d3d2eca43f965d4ead61a369ab0640de
Eric Schmidt <eric@eric.casa>
2022-12-12 15:05:42 +0100

    Completed Day 6

---
+++ Day6/Part2.tcl
@@ -0,0 +1,25 @@
+#!/usr/bin/env tclsh
+
+set input [read [open "input.txt" r]]
+
+set Ergebnis ""
+
+for {set index 13} {$index < [string length $input]} {incr index} {
+    set substr [string range $input [expr $index-13] $index]
+
+    set marker 1
+
+    foreach char [split $substr ""] {
+        if {[string first $char $substr] != [string last $char $substr]} {
+            set marker 0
+            break
+        }
+    }
+    
+    if {$marker == 1} {
+        set Ergebnis [expr $index + 1]
+        break
+    }
+}
+
+puts $Ergebnis
